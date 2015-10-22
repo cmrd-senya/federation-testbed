5f90f62416aa6e021ae80216fba77353a63415c7
cmrd Senya <senya@riseup.net>
2015-10-22 14:24:21 +0300

    For some reason a webfinger query returns 404 for non local contacts.
    Use a local contact here then.

--- features/step_definitions/host_meta_steps.rb
+++ features/step_definitions/host_meta_steps.rb
@@ -4,7 +4,7 @@ end
 
 # MK: What happens in this step does not reflect the name of the step
 Then(/^it should contain a link to the webfinger document$/) do
-  @webfinger_url = @response.to_s.rpartition(/(https?):((\/\/)|(\\\\))+[\w\d:\#\@\%\/;$()~_?\+-=\\\.&]*/)[-2] + "mokus@joindiaspora.com"
+  @webfinger_url = @response.to_s.rpartition(/(https?):((\/\/)|(\\\\))+[\w\d:\#\@\%\/;$()~_?\+-=\\\.&]*/)[-2] + @diaspora_user
   RestClient.get @webfinger_url
 end
 
