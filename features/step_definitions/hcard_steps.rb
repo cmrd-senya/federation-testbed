a6477eb308998f98088791cfedd34d6a078c4e78
cmrd Senya <senya@riseup.net>
2015-10-23 16:42:03 +0300

    Implement hCard validity check

--- features/step_definitions/hcard_steps.rb
+++ features/step_definitions/hcard_steps.rb
@@ -20,11 +20,10 @@ When(/^I make a hcard\-request$/) do
 end
 
 Then(/^the document should contain User profile$/) do
-  response = RestClient.get @webfinger.xpath('//xmlns:Link')[0].attr('href')
-  response.to_s.index("User profile") > 0
+  @response.to_s.index("User profile") > 0
 end
 
 Then(/^I should receive a valid hcard document$/) do
-  pending # express the regexp above with the code you wish you had
+  @h_card = DiasporaFederation::Discovery::HCard.from_html(@response)
 end
 
