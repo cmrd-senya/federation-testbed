8804c41ea4e505250a761ed017cd0fc93212b0a5
cmrd Senya <senya@riseup.net>
2015-10-28 04:41:52 +0300

    Refactor GeneratedData a little

--- lib/federation-testbed/test_data.rb
+++ lib/federation-testbed/test_data.rb
@@ -28,42 +28,62 @@ end
 class FederationTestbed::GeneratedData
   include Singleton
   def test_user_name
-    @user_list.last.username
+    user_list.last.username
   end
 
   def test_user_id
-    @user_list.last.id
+    user_list.last.id
   end
 
   def private_key
-    @user_list.last.private_key
+    user_list.last.private_key
   end
 
   def public_key
-    @user_list.last.public_key
+    user_list.last.public_key
   end
 
   def guid
-    @user_list.last.guid
+    user_list.last.guid
   end
 
   def initialize
-    @user_list = []
+    self.user_list = []
     roll
   end
 
+  attr_accessor :user_list
+  delegate :push, :to => :user_list
+
   def roll
-     @user_list.push(FederationTestbed::User.new)
+    push(FederationTestbed::User.new).last
   end
 
-  def find_by_id_or_create(id)
-    @user_list.each do |u|
+  def find_by_id(id)
+    user_list.each do |u|
       if u.id == id
         return u
       end
     end
+    nil
+  end
+
+  def find_by_id_or_create(id)
+    u = find_by_id(id)
+    return u unless u.nil?
     roll
-    @user_list.last.id = id
-    @user_list.last
+    user_list.last.id = id
+    user_list.last
+  end
+
+  def find_by_guid_or_create(guid)
+    user_list.each do |u|
+      if u.guid == guid
+        return u
+      end
+    end
+    roll
+    user_list.last.guid = id
+    user_list.last
   end
 end
